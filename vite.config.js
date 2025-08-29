9b8d7b5a7bab471ac87f45d57cf330558e954706
iqcinema <iq.cinema.production@gmail.com>
2025-08-29 18:14:22 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/video-cost-calculator/', // ← Меняйте на имя вашего репозитория
-  build: {
-    outDir: 'dist'
-  }
+ plugins: react(),
+ base: '/video-cost-calculator/', // ← имя репозитория
+ build: {
+ outDir: 'dist'
+ },
 });
